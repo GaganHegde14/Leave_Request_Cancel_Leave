babed3d987fb0297ea9ab5dc8ee0ac54b682554b
Gagan Hegde <gbhegde14@gmail.com>
2025-09-17 17:06:35 +0530

    some minor padding changes

--- Leave/src/App.jsx
+++ Leave/src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar.jsx";
 import PaternityLeave from "./components/PaternityLeave.jsx";
 import ApplyLeave from "./components/ApplyLeave.jsx";
 import CancelLeave from "./components/CancelLeave.jsx";
-import Avatars4 from "./components/Avatars4.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
           {/* <Route path="/cancel-leave" element={<CancelLeave />} /> */}
           <Route path="/ApplyLeave" element={<ApplyLeave />} />
           <Route path="/PaternityLeave" element={<PaternityLeave />} />
-          <Route path="/Avatars4" element={<Avatars4 />} />
         </Routes>
       </Router>
     </>
